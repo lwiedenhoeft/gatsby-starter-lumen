a94bd076a02d98c341b0315657608dc8b22cccbf
Lars Wiedenhoeft <lars.wiedenhoeft@siemens.com>
2021-01-02 22:08:48 +0100

    delete travis

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import type { Node as ReactNode } from 'react';
+import * as React from 'react';
 import { useSiteMetadata } from '../../hooks';
 import styles from './Layout.module.scss';
 
